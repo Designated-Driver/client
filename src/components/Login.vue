a80f456872af12c91b82b7a3a094fabec5f89316
Daniel Stansberry <stansberry2295@gmail.com>
2018-01-22 15:48:36 -0800

    tracking the login page

---
+++ src/components/Login.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="login">
+    This is the login page
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Login'
+  }
+</script>
+
+<style scoped>
+.login {
+  height: 100%;
+  width: 100%;
+}
+</style>
