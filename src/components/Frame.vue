0589d48a303281c4ad2b160611fc6ec56339fdf5
Daniel Stansberry <danielsstansberry@gmail.com>
2018-01-29 15:21:45 -0800

    update to the way the sidebar is presented + the frame

--- src/components/Frame.vue
+++ src/components/Frame.vue
@@ -1,49 +1,54 @@
 <template>
-  <div class="frame">
-    <div class="frame-right"> 
-      <router-link to ="/">
-        <span>Home</span>
-      </router-link>  
-      <router-link to="/login">
-        <span>Login</span>
-      </router-link>
-      <router-link to="/signup">
-        <span>Signup</span>
-      </router-link>
+  <div>
+    <div class="frame">
+      <div class="frame-left" @click="showSidebar = !showSidebar">
+        <i class="fa fa-bars fa-lg" aria-hidden="true" v-if="!showSidebar"></i>
+        <i class="fa fa-times fa-lg" aria-hidden="true" v-else></i>        
+      </div>
+      <div class="frame-right">
+        DesignatedDriver
+      </div>
     </div>
+    <transition name="sidebar">
+    
+    </transition>
+    <sidebar v-if="showSidebar"/>
   </div>
 </template>
 
 <script>
+  import Sidebar from './Frame/Sidebar'
   export default {
+    data () {
+      return {
+        showSidebar: false
+      }
+    },
+    components: { Sidebar }
   }
 </script>
 
 <style lang="scss" scoped>
 .frame {
-  margin: 0;
   height: 45px;
-  padding: 0 50px;
+  width: 100%;
   background-color: #35495E;
   color: #ffffff;
   display: flex;
   justify-content: space-between;
-}
+  z-index: 0;
 
+  .frame-left {
+    width: 45px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
 
-.frame-right {
-  height: 100%;
-  width: 200px;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-}
-
-.frame-right a {
-  text-decoration: none;
-}
-
-.frame-right a span {
-  color: white;
+  .frame-right {
+    display: flex;
+    align-items: center;
+    padding-right: 10px;
+  }
 }
 </style>
---
+++ src/components/Frame/Sidebar.vue
@@ -0,0 +1,65 @@
+<template>
+<transition name="sidebar">
+  <div class="sidebar">
+    <div class="sidebar-nav"> 
+      <router-link to ="/">
+        <span>Home</span>
+      </router-link> 
+      <router-link to ="/about">
+        <span>About</span>
+      </router-link>  
+      <router-link to="/login">
+        <span>Login</span>
+      </router-link>
+      <router-link to="/signup">
+        <span>Signup</span>
+      </router-link>
+    </div>
+  </div>
+</transition>
+  
+</template>
+
+<script>
+  export default {
+    name: 'Sidebar'
+  }
+</script>
+
+<style lang="scss" scoped>
+.sidebar {
+  position: absolute;
+  height: 100vh;
+  width: 66vw;
+  background-color: #35495E;
+  z-index: 1;
+
+  .sidebar-nav {
+    width: 80%;
+    height: 50%;
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    padding: 0 10%;
+
+    a {
+      height: 40px;
+      display: flex;
+      align-items: flex-end;
+      text-decoration: none;
+
+      span {
+        color: white;
+      }
+    }
+  }
+}
+
+.sidebar-enter-active, .sidebar-leave-active {
+  transition: all ease-in-out .2s;
+}
+
+.sidebar-enter, .sidebar-leave-to /* .fade-leave-active below version 2.1.8 */ {
+  width: 0;
+}
+</style>
