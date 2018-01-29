cf8342a5bb6a3b2c62d63be09a28e2cfd75886bb
Daniel Stansberry <danielsstansberry@gmail.com>
2018-01-29 15:22:48 -0800

    cleaned up login page to reduce overflow

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,21 +1,18 @@
 <template>
-<center>
   <div class="login">
-  Log in
+    Log in
     <div class="box">
-    Username:
-    <input type="text" name="user">
+      Username:
+      <input type="text" name="user">
     </div>
     <div class="box">
-    Password: 
-    <input type="password" name="password">
-    
+      Password: 
+      <input type="password" name="password">
     </div>
     <router-link to ="/submit">
       <span><input type="submit" value="Submit"></span>
     </router-link>
   </div>
-</center>
 </template>
 
 <script>
@@ -24,24 +21,21 @@
   }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
 .login {
-  width: 400px;
-  height: 150px; 
-  border: 1px solid green;
-  padding-top: 50px;
-  padding-left: 15px;
-  
-}
-.box{
-  width:100%;
-  display:flex;
-  align-items:center;  
-}
-input{
-  width:250px;
-
-}
+  width: 100vw;
+  height: 100%; 
 
 
+  .box{
+    width: 80%;
+    padding: 0 10%;
+    display:flex;
+    align-items:center;
+    
+    input{
+      max-width: 250px;
+    }
+  } 
+}
 </style>
