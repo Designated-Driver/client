4f39fdd15064f592812d27f4c51157a1555d0660
Daniel Stansberry <danielsstansberry@gmail.com>
2018-01-30 11:31:11 -0800

    including about page

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/components/Home'
+import About from '@/components/About'
 import Login from '@/components/Login'
 import Signup from '@/components/Signup'
 import Submit from '@/components/Submit'
@@ -12,6 +13,11 @@ export default new Router({
       path: '/',
       name: 'Home',
       component: Home
+    },
+    {
+      path: '/about',
+      name: 'About',
+      component: About
     },
     {
       path: '/login',
