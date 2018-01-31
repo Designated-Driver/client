b89141e82bb21864d185c06769339855fb66d2c3
Daniel Stansberry <danielsstansberry@gmail.com>
2018-01-31 15:39:36 -0800

    modification to home/signup page, need to work on auth next

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -3,8 +3,8 @@
     <div class="main">
       This is the main map.
     </div>
-    <div class="toolbar" :style="{'height': (showLogIn || showSignIn) ? '50%' : '10%' }">
-      <template v-if="(!showLogIn && !showSignIn)">
+    <div class="toolbar" :style="{'height': (showLogIn || showSignUp) ? '50%' : '10%' }">
+      <template v-if="(!showLogIn && !showSignUp)">
         <span @click="clickLogin">Login</span>
         <span @click="clickSignUp">Signup</span>
       </template>
@@ -12,8 +12,16 @@
         <div class="login" v-if="showLogIn">
           This is the login
         </div>
-        <div class="signup" v-else-if="showSignIn">
-          This is the signup
+        <div class="signup" v-else-if="showSignUp">
+          <h1>Create An Account</h1>
+          <form @submit.prevent="submitSignUp()">
+            <div>
+              <input v-model="fullName" type="text" placeholder="Full Name">              
+              <input v-model="email" type="text" placeholder="Email Address">
+              <input v-model="password" type="password" placeholder="Password">
+            </div>
+            <button type="submit">Submit</button>
+          </form>
         </div>
       </template>
     </div>
@@ -25,8 +33,11 @@ export default {
   name: 'home',
   data () {
     return {
-      showSignIn: false,
-      showLogIn: false
+      showSignUp: true,
+      showLogIn: false,
+      email: '',
+      password: '',
+      fullName: ''
     }
   },
   methods: {
@@ -34,7 +45,18 @@ export default {
       this.showLogIn = true
     },
     clickSignUp () {
-      this.showSignIn = true
+      this.showSignUp = true
+    },
+    submitSignUp () {
+      console.log('you submit the form')
+      if (!this.email.includes('@')) {
+        console.log('this email doesnt contain @')
+        // show on UI the email needs to contain @
+      }
+      this.showSignUp = false
+      // catch other conditions
+      // otherwise
+      // submit to firebase auth
     }
   }
 }
@@ -74,6 +96,57 @@ export default {
       justify-content: center;
       align-items: center;
       border: 2px solid black;
+    }
+
+    .signup {
+      width: 90%;
+      height: 90%;
+      // border: 1px solid black;
+      display: flex;
+      flex-direction: column;
+
+      h1 {
+        // border: 1px solid black;
+        height: 48px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        font-size: 32px;
+      }
+
+      form {
+        // border: 1px solid black;
+        flex-grow: 1;
+        position: relative;
+        div {
+          margin-top: 20px;
+          height: 175px;
+          display: flex;
+          flex-direction: column;
+          align-items: center;
+          justify-content: space-between;
+
+          input {
+            width: 80%;
+            border: 2px solid gray;
+            background: white;
+            height: 28%;
+            font-size: 100%;
+            padding-left: 10px;
+          }
+        }
+
+        button {
+          position: absolute;
+          bottom: 20px;
+          right: 20px;
+          border: 2px solid black;
+          background: white;
+          width: 120px;
+          height: 35px;
+        }
+      }
+
     }
   }
 }
--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/components/Home'
-import About from '@/components/About'
-import Login from '@/components/Login'
-import Signup from '@/components/Signup'
-import Submit from '@/components/Submit'
-import Ccinfo from '@/components/Ccinfo'
 Vue.use(Router)
 
 export default new Router({
@@ -14,31 +9,6 @@ export default new Router({
       path: '/',
       name: 'Home',
       component: Home
-    },
-    {
-      path: '/about',
-      name: 'About',
-      component: About
-    },
-    {
-      path: '/login',
-      name: 'Login',
-      component: Login
-    },
-    {
-      path: '/signup',
-      name: 'Signup',
-      component: Signup
-    },
-    {
-      path: '/submit',
-      name: 'Submit',
-      component: Submit
-    },
-    {
-      path: '/ccinfo',
-      name: 'cinfo',
-      component: Ccinfo
     }
   ]
 })
