09b63688242491f7536ddda92e0acbbe5b56c70b
Daniel Stansberry <danielsstansberry@gmail.com>
2018-02-15 17:28:38 -0800

    find friends tab udpate

---
+++ src/components/Toolbar/AuthToolbar/RiderToolbar/RiderFriends/FriendSelect/ShowFriends.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="show-friends">
+    This is the show friends component
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'ShowFriends'
+  }
+</script>
+
+<style lang="scss" scoped>
+.show-friends {
+  height: calc(100% - 50px);
+  width: calc(100% - 50px);
+  background: red;
+}
+</style>
