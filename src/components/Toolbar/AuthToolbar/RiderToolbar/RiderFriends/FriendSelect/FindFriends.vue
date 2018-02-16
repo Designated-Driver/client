4fa0245570ccc2ad6b29f899a1383447d1002a27
Daniel Stansberry <danielsstansberry@gmail.com>
2018-02-16 00:31:16 -0800

    including files that were missing bad commit sorry

---
+++ src/components/Toolbar/AuthToolbar/RiderToolbar/RiderFriends/FriendSelect/FindFriends.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="find-friends">
+    This is the find friends component
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'FindFriends'
+  }
+</script>
+
+<style lang="scss" scoped>
+.find-friends {
+  height: calc(100% - 50px);
+  width: calc(100% - 50px);
+  background: red;
+}
+</style>
