4f42b6f98560a23a14497130207a15e716b54619
Katerina Chinnappan <agukalpa@eduroam-169-233-242-72.ucsc.edu>
2018-04-12 14:54:56 -0700

    working on payment

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/components/Home'
-import Card from '@/components/Toolbar/RiderRquest/Card'
 Vue.use(Router)
 
 export default new Router({
@@ -11,11 +10,6 @@ export default new Router({
       path: '/',
       name: 'Home',
       component: Home
-    },
-    {
-      path: '/card',
-      name: 'Card',
-      component: Card
     }
   ]
 })
