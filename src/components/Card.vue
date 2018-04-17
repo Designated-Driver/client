6507e390d07807c1f05c4f4a4e92d877cc50a125
Katerina Chinnappan <agukalpa@eduroam-169-233-229-69.ucsc.edu>
2018-04-17 12:51:02 -0700

    confirmation popup for payment

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -5,23 +5,40 @@
     <toolbar/>
   </div>
   <br></br>
+  <div class="card">
+  <center>
     <div :class="wrapperClass">
+    <div class="trip-total" v-if="getTripCost">
+          <span>Trip Total: ${{getTripCost.toFixed(2).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}}</span>    
+        </div> 
     <div id="dropin-container"></div>
     
     <center>
-    <button type="submit" style="padding-top: 1rem;" id="submitTransaction" @click="dropinRequestPaymentMethod">Test to pay</button>
-    <button type="submit" style="padding-top: 1rem;" @click="goHome">Home</button>
+    <div class="popup">
+    <button type="submit" style="padding-top: 1rem; background: black;" id="submitTransaction" @click="confirm">Confirm to Pay</button>
+     <span class="popuptext" id="popupp" @click="dropinRequestPaymentMethod">Yes</span>
+    </div>
+    <button type="submit" style="padding-top: 1rem; background: black;" @click="goHome"> Return Home</button>
+
     </center>
   </div>
-
+  </center>
+</div>
   </div>
  </template>
 
 <script src="https://js.braintreegateway.com/web/dropin/1.10.0/js/dropin.min.js"></script> 
 <script>
   import Toolbar from './Toolbar/CardToolbar'
+  import { mapGetters } from 'vuex'
   export default {
     name: 'card',
+    computed: {
+      ...mapGetters([
+        'getTripCost'
+      ])
+
+    },
     components: {Toolbar},
     props: {
       authToken: {
@@ -94,6 +111,10 @@
       goHome: function () {
         this.$router.push('/')
       },
+      confirm: function () {
+        var popup = document.getElementById('popupp')
+        popup.classList.toggle('show')
+      },
       dropinRequestPaymentMethod: function () {
         this.dropinInstance.requestPaymentMethod((requestErr, payload) => {
           if (requestErr) {
@@ -118,7 +139,66 @@
   width: 100%;
 }
 .card{
+  padding-top: 1rem;
+  width: 50%;
+  position:absolute;
+  margin-left:350px;
 
+}
+.popup {
+    position: relative;
+    display: inline-block;
+    cursor: pointer;
+    -webkit-user-select: none;
+    -moz-user-select: none;
+    -ms-user-select: none;
+    user-select: none;
+}
+/* The actual popup (appears on top) */
+.popup .popuptext {
+    visibility: hidden;
+    width: 160px;
+    background-color: #555;
+    color: #fff;
+    text-align: center;
+    border-radius: 6px;
+    padding: 8px 0;
+    position: absolute;
+    z-index: 1;
+    bottom: 50%;
+    height:50%;
+    left: 50%;
+    margin-left: -80px;
+}
+
+/* Popup arrow */
+.popup .popuptext::after {
+    content: "";
+    position: absolute;
+    top: 100%;
+    left: 50%;
+    margin-left: -5px;
+    border-width: 5px;
+    border-style: solid;
+    border-color: #555 transparent transparent transparent;
+}
+
+/* Toggle this class when clicking on the popup container (hide and show the popup) */
+.popup .show {
+    visibility: visible;
+    -webkit-animation: fadeIn 1s;
+    animation: fadeIn 1s
+}
+
+/* Add animation (fade in the popup) */
+@-webkit-keyframes fadeIn {
+    from {opacity: 0;} 
+    to {opacity: 1;}
+}
+
+@keyframes fadeIn {
+    from {opacity: 0;}
+    to {opacity:1 ;}
 }
 </style>
 
